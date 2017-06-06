dcd0b261df1523d9df284154d959215c38c8aa8a
LinghanX <linghan@me.com>
2017-06-06 12:06:55 -0400

    Implemented sortable directive

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -2,22 +2,37 @@ const app = require('../../express');
 const Helper = require('../helpers/generateId');
 
 const widgets = [
-    { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
-    { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+    { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO", "index": 1},
+    { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum", "index": 2},
     { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-        "url": "http://lorempixel.com/400/200/"},
-    { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-    { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        "url": "http://lorempixel.com/400/200/", "index": 3},
+    { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>", "index": 4},
+    { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum", "index": 5},
     { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-        "url": "https://youtu.be/AM2Ivdi9c4E" },
-    { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+        "url": "https://youtu.be/AM2Ivdi9c4E", "index": 6},
+    { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>", "index": 7}
 ];
 
+app.post('/api/assignment/page/:pageId/widget/order', reOrderWidget);
 app.post('/api/assignment/page/:pageId/widget', createWidget);
 app.get('/api/assignment/page/:pageId/widget', findAllWidgetsForPage);
 app.get('/api/assignment/widget/:widgetId', findWidgetById);
 app.put('/api/assignment/widget/:widgetId', updateWidget);
 app.delete('/api/assignment/widget/:widgetId', deleteWidget);
+
+function reOrderWidget(req, res){
+    const pageId = req.params['pageId'];
+    const newOrder = req.body.elems;
+    for(var i in newOrder){
+        for(var j in widgets){
+            if(widgets[j]._id == newOrder[i]){
+                widgets[j].index = i;
+            }
+        }
+    }
+
+    res.sendStatus(200);
+}
 
 function deleteWidget(req, res) {
     const widgetId = req.params['widgetId'];
---
+++ public/assignment/directives/webdevSortable.js
@@ -0,0 +1,31 @@
+(function(){
+    angular.module('WebAppMaker').directive('sortable', elementSortable);
+
+    function elementSortable(){
+        function linkFunction(scope, element) {
+            const pageId = scope.pageId;
+            $(element).sortable({
+                update: function(event, ui){
+                    const elemOrder = [];
+                    const widgetsElem = $('.widget').toArray();
+                    widgetsElem.forEach(function(item){
+                        elemOrder.push(item.id);
+                    });
+
+                    $.post(
+                        '/api/assignment/page/' + pageId + '/widget/order',
+                        {elems: elemOrder}
+                    );
+                }
+                }
+            );
+        }
+
+        return {
+            scope: {
+                pageId: '=pid'
+            },
+            link: linkFunction
+        }
+    }
+})();
